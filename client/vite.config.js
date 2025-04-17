b4ecac82aec692996580c76221bc4da99120fa33
Adarsh Chauhan <117521808+Adarshchauhan77@users.noreply.github.com>
2025-04-17 14:11:18 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()  
+  ],
   define: {
-    global: "globalThis",
-    "process.env": {},
+    global: "globalThis", 
+    "process.env": {},   
   },
 });
